edeeeda3dd8a05a52d8ced0e5999b12b0ee31101
EnriqueTor <etorrendell@gmail.com>
2016-09-14 09:57:07 -0400

    Complete Problems

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -12,11 +12,98 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    /*
-     
-     write your function here
-     
-     */
+    func solveTrivia(dic:[String:String]) -> String {
+        var state = String()
 
+        for (key,value) in dic {
+            
+            for char in key.characters {
+                
+                if value.lowercaseString.characters.contains(char) {
+                    print("not me")
+                }
+                else {
+                    state.append(char)
+                }
+            }
+        }
+        return state
 }
+}
+
+
+
+//        for (key,value) in dic {
+//
+//                for char in key.characters {
+//
+//                    for c in value.characters {
+//                        
+//                        if char.hashValue == c.hashValue {
+//                            print("not me")
+//                        }
+//                        else {
+//                            print(char)
+//                            state.append(char)
+//                        }
+//                    }
+//
+//                    if state != key {
+//
+//                        state = ""
+//}
+//}
+//}
+//return state
+//}
+//}
+
+
+
+//        for (key,value) in dic {
+//            
+//            if Array(key.lowercaseString.characters) != Array(value.lowercaseString.characters) {
+//                
+//                
+//            }
+//}
+
+//        for key in dic.keys {
+//                
+//                for char in key.lowercaseString.characters {
+//                    
+//                    for value in dic.values {
+//                    
+//                        for c in value.lowercaseString.characters {
+//                        
+//                            if c != char {
+//                            
+//                            state.append(char)
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return state
+//    }
+//}
+
+
+//        for (key,value) in dic {
+//            
+//            for char in key.lowercaseString.characters {
+//                
+//                for c in value.lowercaseString.characters {
+//                    
+//                    if char.hashValue != c.hashValue {
+//                        
+//                        state = key
+//                    }
+//                }
+//            }
+//        }
+//        return state
+//    }
+//}
+//
 
